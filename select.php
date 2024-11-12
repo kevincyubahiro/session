7a77caf50abfb7d309480bafa0e1f6b272e4c035
kevincyubahiro <kevincyubahiro250@gmail.com>
2024-11-12 09:27:51 -0800

    crud for php

---
+++ select.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1">
+<tr>
+<th>id</th>
+<th>username</th>
+<th>password</th>
+
+</tr>
+<?php 
+include("conn.php");
+$select=mysqli_query($conn,"SELECT * FROM trainers")
+while($row=mysqli_fetch_array($select)){
+
+?>
+<tr>
+    <td><</td>
+</tr>
+
+    </table>
+</body>
+</html>
