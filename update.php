6322df574af1358d4f97c116a6cec95e9f1e1056
kevincyubahiro <kevincyubahiro250@gmail.com>
2024-11-12 09:42:13 -0800

    crud for php

--- update.php
+++ update.php
@@ -13,7 +13,27 @@ $row=mysqli_fetch_array($select)
 </head>
 <body>
     <form action="" method="post">
-        
+    <input type="text"name ="username"placeholder="username"values="<?php echo $row['username']?>">
+        <input type="password"name ="password"placeholder="password"values="<?php echo $row['password']?>">
+        <button name="submit">update</button>
     </form>
 </body>
 </html>
+<?php
+include("conn.php");
+if(isset($_POST['submit'])){
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    $insert = mysqli_query($conn, "INSERT INTO trainers (username, password) VALUES ('$username', '$password')");
+
+
+    if($insert){
+        echo"data inserted";
+    }
+    else{
+        echo"not data inserted";
+    }
+}
+
+
+?>
