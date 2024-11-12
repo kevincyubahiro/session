7b37d0e913b9f22ce2f8c254888f981f6ecdf735
kevincyubahiro <kevincyubahiro250@gmail.com>
2024-11-12 09:01:13 -0800

    crud for php

--- insert.php
+++ insert.php
@@ -6,6 +6,9 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="" method="post">
+        <input type="text"name ="username"placeholder="username">
+        <input type="password"name ="password"placeholder="password">
+    </form>
 </body>
 </html>
